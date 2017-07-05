c6b2ed700450e8648c87b6ffeae6f5a27b8845cc
emilyb22 <emilyb22@live.ca>
2017-07-05 16:27:25 -0400

    adding the local storage component

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 angular.module('ToDo',[]). /*App controller*/
-controller('todoController',['$scope',function($scope){ /*displaying list controller*/
-    $scope.todos = [
-        {'title':'Build a todo app', 'date':'July 4th' ,'done':false}
-    ];
+controller('todoController',['$scope','myLocalStorage', function($scope, myLocalStorage){ /*displaying list controller*/
+    console.log(myLocalStorage)
+    $scope.todos = myLocalStorage.get("item") || [];
     
+    console.log($scope.todos)
     $scope.addTodo = function(){ /*Adding items through the formy input*/
         $scope.todos.push({'title':$scope.newTodo,'date':$scope.newDate, 'done':false}) //adding the date and the title
+        myLocalStorage.set("item", $scope.todos)
         $scope.newTodo = ''/*To empty the input*/
         $scope.newDate = ''
     }
@@ -15,17 +16,8 @@ controller('todoController',['$scope',function($scope){ /*displaying list contro
             $scope.todos = $scope.todos.filter(function(item){
             return !item.done
         })
+            myLocalStorage.remove('item', $scope.todos)
+            myLocalStorage.set('item', $scope.todos)
     }
     
-}]) //controller todo
-    
-
-/*addnew (){
-$scope.todoList.push(newTodo)
-syncLocalStorage()
-resetNewTodo()
-    }
-syncLocalStorage(){
-localStorage.setItem('todoList', JSON.stringify($scope.todoList))
-}    
-*/
+}]) 
